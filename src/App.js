5988af733d676d0124cb63f97701f648b41508d4
Aamer Ali <sayyed.aamer.ali@gmail.com>
2021-09-21 19:02:01 +0530

    LoginForm Created.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Rentals from "./components/rentals";
 import Customers from "./components/customers";
 import React from "react";
 import MovieForm from "./components/movieForm";
+import LoginForm from './components/loginForm';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <main className="container">
         <div className="container">
           <Switch>
+          <Route path="/login/" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movie} />
             <Route path="/customers" component={Customers} />
